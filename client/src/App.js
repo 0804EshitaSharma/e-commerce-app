1c77cf1be1960f0ad2d41d711d2bd4dac6169509
Danielle Chen <jt17@students.cs.ubc.ca>
2023-06-06 23:15:45 -0700

    added category nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import Home from './pages/HomeProducts';
+import Electronics from './pages/Electronics';
+import Books from './pages/Books';
+import Fashion from './pages/Fashion';
 
 function App() {
   return (
-    <div className="App">
-    <Navbar/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* <Route path='/' exact component={Home} /> */}
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/Electronics' element={<Electronics />} />
+        <Route path='/Books' element={<Books />} />
+        <Route path='/Fashion' element={<Fashion />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CategoryContainer from "../components/Dashboard/Categories/CategoryContainer";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <CategoryContainer />
+        </div>
+    )
+}
+
+export default Dashboard
