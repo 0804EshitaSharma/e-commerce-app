c05418b7ad365f35cee03dfc9e2307eee58fdc46
ellee311 <elsie.lee1214@gmail.com>
2023-06-08 02:25:10 -0700

    added shopping cart page with navbar link to it

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
 function Navbar() {
-const { name } = useParams();
+  const { name } = useParams();
   return (
     <div className="navbar">
       <span className="navbar_title">E-Commerce</span>
@@ -63,21 +63,23 @@ const { name } = useParams();
         </svg>
 
         <div className="navbar_link_3">
-          <svg
-            className="navbar_shopping-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-            />
-          </svg>
-          <span className="navbar_shopping-icon_count">0</span>
+          <Link to="/shoppingCart">
+            <svg
+              className="navbar_shopping-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+            <span className="navbar_shopping-icon_count">0</span>
+          </Link>
         </div>
       </div>
     </div>
