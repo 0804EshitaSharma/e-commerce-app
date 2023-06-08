5280b62f1ab70d32a3ca6ba9d9d5047cecb262d5
Danielle Chen <jt17@students.cs.ubc.ca>
2023-06-08 14:59:47 -0700

    changes after merge

--- client/src/App.js
+++ client/src/App.js
@@ -15,19 +15,22 @@ import Fashion from './pages/Fashion';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/:name" element={<Navbar />}></Route>
-        <Route path="/login/" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/checkout" element={<Checkout />}></Route>
-        <Route path="/shoppingCart" element={<Cart />}></Route>
-        <Route path="*" element={<Navbar />}></Route>
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/Home' element={<Home />} />
-        <Route path='/Electronics' element={<Electronics />} />
-        <Route path='/Books' element={<Books />} />
-        <Route path='/Fashion' element={<Fashion />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/:name" element={<Navbar />}></Route>
+          <Route path="/login/" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
+          <Route path="/shoppingCart" element={<Cart />}></Route>
+          <Route path="*" element={<Navbar />}></Route>
+          <Route path='/dashboard' element={<Dashboard />} ></Route>
+          <Route path='/Home' element={<Home />} />
+          <Route path='/Electronics' element={<Electronics />} ></Route>
+          <Route path='/Books' element={<Books />} ></Route>
+          <Route path='/Fashion' element={<Fashion />} ></Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
