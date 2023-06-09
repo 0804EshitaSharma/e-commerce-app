2c6eeacd8f6022ee27770b0cf971ef91e8da7ff6
ellee311 <elsie.lee1214@gmail.com>
2023-06-09 02:05:23 -0700

    cleaned up imports and put Cart into page folder

--- client/src/components/User/Login.js
+++ client/src/components/User/Login.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import Navbar from "../Navbar/Navbar.js";
 import Form from "../Custom/Form";
 import "./Login.css";
 function Login() {
