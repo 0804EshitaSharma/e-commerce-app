b08e7f9f4062021772093c72fd56621d3fc50dde
eshitasharma <eshitasharma0804@gmail.com>
2023-06-09 12:14:56 +0530

    resolved-conflicts

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import CategoryContainer from "../components/Dashboard/Categories/CategoryContainer";
 import ContentContainer from "../components/Dashboard/ContentContainer";
-import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
     return (
