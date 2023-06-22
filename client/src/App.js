dcfb809dc3b7ad7d7b82e026b843d47b7b00158b
eshitasharma <eshitasharma0804@gmail.com>
2023-06-22 13:08:06 -0700

    fixed-password-reset-issue

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ import Books from "./pages/Books";
 import Fashion from "./pages/Fashion";
 import UserProfile from "./components/User/UserProfile";
 import PrivateRoutes from "./utils/PrivateRoutes";
-import PasswordReset from "./components/User/PasswordReset";
 
 function App() {
   const [userName, setUsername] = useState("");
@@ -49,7 +48,7 @@ function App() {
           <Route path="/fashion" element={<Fashion />}></Route>
           <Route path="/" element={<Dashboard />}></Route>
           <Route path="/user" element={<UserProfile />}></Route>
-          <Route path="/reset-password" element={<PasswordReset />}></Route>
+    
         </Routes>
       </Router>
     </div>
