3dec76bac80756d5fe22f8c9bbba9a334c6655e8
Danielle Chen <jt17@students.cs.ubc.ca>
2023-06-21 23:39:32 -0700

    update prod info, add ability to filter category

--- client/src/components/Dashboard/Filters/index.js
+++ client/src/components/Dashboard/Filters/index.js
@@ -1,52 +1,15 @@
 import FilterCard from "./filterCard"
+import { filters } from "./filterLists";
 
-const filters = {
-    list: [
-        {
-            name: "Category",
-            category: [
-                "Home",
-                "Electronics",
-                "Books",
-                "Fashion"
-            ]
-        },
-        {
-            name: "Price",
-            category: [
-                "Under $25",
-                "$25 ~ $50",
-                "$50 ~ $100",
-                "$100 ~ $200",
-                "Above $200"
-            ]
-        },
-        {
-            name: "Rating",
-            category: [
-                "Above 4",
-                "3 ~ 4",
-                "2 ~ 3",
-                "1 ~ 2",
-                "Below 1"
-            ]
-        },
-        {
-            name: "Deals & Discounts",
-            category: [
-                "All Discounts",
-                "Today's Deal"
-            ]
-        }
-    ]
-}
+export default function Filters(props) {
+    const categories= props.categories // [{ id, checked, label }]
 
-export default function Filters() {
     return (
         <div className="flexbox-container">
             <div>
-                {filters.list.map((filter) => <FilterCard key={filter.name} filter={filter}/>)
-                }
+                {/* {filters.list.map((filter) => <FilterCard key={filter.name} filter={filter}/>)
+                } */}
+                <FilterCard name='Categories' filter={categories} changeChecked={props.changeChecked}/>
             </div>
 
         </div>
