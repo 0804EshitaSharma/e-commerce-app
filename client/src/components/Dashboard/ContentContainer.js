2b6cd9b75e2bf7dc2f7f615ff1aed25f4449b1e9
Danielle Chen <jt17@students.cs.ubc.ca>
2023-06-21 23:39:32 -0700

    update prod info, add ability to filter category

--- client/src/components/Dashboard/ContentContainer.js
+++ client/src/components/Dashboard/ContentContainer.js
@@ -1,14 +1,62 @@
+// referenced from: https://www.youtube.com/watch?v=VVhnuOKVHRs&t=6210s
+
 import './Dashboard.css'
 import Filters from "./Filters";
 import ProductContainer from './Products/ProductContainer';
+import { useState, useEffect } from "react"
+import { initialState } from './Products/ProdDataList';
 
 export default function ContentContainer() {
+
+    const [resultsFound, setResultsFound] = useState(true);
+    const [list, setList] = useState(initialState.list);
+
+    const [categories, setCategories] = useState([
+        { id: 1, checked: false, label: 'Home' },
+        { id: 2, checked: false, label: 'Electronics' },
+        { id: 3, checked: false, label: 'Books' },
+        { id: 4, checked: false, label: 'Fashion' },
+    ])
+
+    const handleChangeChecked = (id) => {
+        const categoriesStateList = categories;
+        const changeCheckedCategories = categoriesStateList.map((item) =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+        );
+        setCategories(changeCheckedCategories);
+    };
+
+    const applyFilters = () => {
+        let updatedList = initialState.list;
+
+        // Cuisine Filter
+        const categoriesChecked = categories
+            .filter((item) => item.checked)
+            .map((item) => item.label);
+
+        if (categoriesChecked.length) {
+            updatedList = updatedList.filter((item) =>
+            categoriesChecked.includes(item.Category)
+            );
+        }
+
+        setList(updatedList);
+        !updatedList.length ? setResultsFound(false) : setResultsFound(true);
+    }
+
+    useEffect(() => {
+        applyFilters();
+    }, [categories]);
+
     return (
         <div className="horizontal-container">
             <div className="filter-container">
-                <Filters />
+                <Filters 
+                    categories={categories}
+                    changeChecked={handleChangeChecked}
+                />
             </div>
-            <ProductContainer />
+            <ProductContainer list={list}/>
         </div>   
     )
 }
---
+++ client/src/components/Dashboard/Products/ProdDataList.js
@@ -0,0 +1,68 @@
+export const initialState = {
+    list: [
+        {
+          Name: "Home Lego Orchid",
+          Description: "Lego Orchid",
+          Category: "Home",
+          Price: "59.98",
+          Rating: "5",
+          Images: ["https://m.media-amazon.com/images/I/71iY-AO2D1L._AC._SR360,460.jpg"]
+        },
+        {
+          Name: "Home Lego Wildflower Bouquet",
+          Description: "Lego Wildflower Bouquet",
+          Category: "Home",
+          Price: "73.99",
+          Rating: "4.5",
+          Images: ["https://m.media-amazon.com/images/I/91E3Lcl8P2L._AC._SR360,460.jpg"]
+        },
+        {
+          Name: "Home Lego Bonsai Tree Building",
+          Description: "Lego Bonsai Tree Building",
+          Category: "Home",
+          Price: "64.98",
+          Rating: "5",
+          Images: ["https://m.media-amazon.com/images/I/71pVP0qS4wL._AC_UL800_FMwebp_QL65_.jpg"]
+        },
+        {
+          Name: "Home Lego Harry Potter Hogwarts Express",
+          Description: "Lego Harry Potter Hogwarts Express",
+          Category: "Home",
+          Price: "618.98",
+          Rating: "5",
+          Images: ["https://m.media-amazon.com/images/I/81cuoMC1o6L._AC_UL800_QL65_.jpg"]
+        },
+        {
+            Name: "Home Lego Cabin",
+            Description: "Lego Cabin",
+            Category: "Books",
+            Price: "59.98",
+            Rating: "5",
+            Images: ["https://m.media-amazon.com/images/I/81juWZTD2HL._AC_UL800_FMwebp_QL65_.jpg"]
+          },
+          {
+            Name: "Home Lego Toy Car Model",
+            Description: "Lego Toy Car Model",
+            Category: "Home",
+            Price: "23.99",
+            Rating: "4.5",
+            Images: ["https://m.media-amazon.com/images/I/81smQxculNL._AC_UL800_FMwebp_QL65_.jpg"]
+          },
+          {
+            Name: "Electronics Lego Plant Set",
+            Description: "Lego Plant Set",
+            Category: "Electronics",
+            Price: "64.98",
+            Rating: "5",
+            Images: ["https://m.media-amazon.com/images/I/71OywSYVdzL._AC_UL800_FMwebp_QL65_.jpg"]
+          },
+          {
+            Name: "Electronics Lego Magic Unicorn",
+            Description: "Lego Magic Unicorn",
+            Category: "Electronics",
+            Price: "18.98",
+            Rating: "5",
+            Images: ["https://m.media-amazon.com/images/I/717CBYoUL7L._AC_UL800_FMwebp_QL65_.jpg"]
+          }
+      ]
+    };
