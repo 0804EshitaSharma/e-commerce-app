304b613d27fd54e7e490babad5aabedcd009dce8
eshitasharma <eshitasharma0804@gmail.com>
2023-06-21 22:53:34 -0700

    added-code-for-user-update

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -12,9 +12,12 @@ export const userSlice = createSlice({
     loggedOut: (state, action) => {
       state.user = null;
     },
+    updateUserInfo: (state, action) => {
+      state.user = action.payload;
+    },
   },
 });
 
-export const { loggedInUser, loggedOut } = userSlice.actions;
+export const { loggedInUser, loggedOut, updateUserInfo } = userSlice.actions;
 
 export default userSlice.reducer;
