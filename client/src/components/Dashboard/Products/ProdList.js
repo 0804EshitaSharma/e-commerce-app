601d176db51715e79cc90e133f4a712fc166dec6
Danielle Chen <jt17@students.cs.ubc.ca>
2023-06-23 14:33:07 -0700

    remove category nav bar

--- client/src/components/Dashboard/Products/ProdList.js
+++ client/src/components/Dashboard/Products/ProdList.js
@@ -2,6 +2,8 @@ import ProductCard from "./ProductCard";
 import { ProdListContainer } from "../../Styles/ProdListContainer.styled";
 
 export default function ProductList(props) {
+  console.log('Product List')
+  console.log(props.list)
     return (
         <ProdListContainer>
             {props.list.map((item) => (
--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,11 +1,11 @@
 import React from "react";
-import CategoryContainer from "../components/Dashboard/Categories/CategoryContainer";
+// import CategoryContainer from "../components/Dashboard/Categories/CategoryContainer";
 import ContentContainer from "../components/Dashboard/ContentContainer";
 
 const Dashboard = () => {
     return (
         <div>
-            <CategoryContainer />
+            {/* <CategoryContainer /> */}
             <ContentContainer />
         </div>
     )
