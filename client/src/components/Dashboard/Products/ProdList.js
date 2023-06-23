fa50efdb1a4b616b19d7cb4f68e8fd0a10118a45
Danielle Chen <jt17@students.cs.ubc.ca>
2023-06-23 15:26:25 -0700

    removed print statement

--- client/src/components/Dashboard/Products/ProdList.js
+++ client/src/components/Dashboard/Products/ProdList.js
@@ -2,8 +2,6 @@ import ProductCard from "./ProductCard";
 import { ProdListContainer } from "../../Styles/ProdListContainer.styled";
 
 export default function ProductList(props) {
-  console.log('Product List')
-  console.log(props.list)
     return (
         <ProdListContainer>
             {props.list.map((item) => (
--- client/src/components/Dashboard/Products/ProductContainer.js
+++ client/src/components/Dashboard/Products/ProductContainer.js
@@ -2,8 +2,6 @@ import ProductList from "./ProdList"
 import ProductSlideShow from "./ProdSlideShow"
 
 export default function ProductContainer(props) {
-    console.log('Product Container')
-    console.log(props.list)
     return (
         <div style={{width: '85%'}}>
             <ProductSlideShow />
