36d644a7e78b6ac9fe9421f61fd050d0196a4bf6
ellee311 <elsie.lee1214@gmail.com>
2023-06-23 17:53:51 -0700

    styling changes to add to cart button on prod card

--- client/src/components/Dashboard/Products/ProductCard.js
+++ client/src/components/Dashboard/Products/ProductCard.js
@@ -14,41 +14,51 @@ export default function ProductCard(props) {
     imgURLs: props.item.Images,
   };
   const quantity = 1;
-    return (
-      <ProdCard key={props.item.Name}>
-        <div style={{ backgroundColor: 'white' }}>
-          <Link to="/product">
-            <img style={{ width: "100%", height: "200px", objectFit: 'contain' }} src={props.item.Images[0]} alt="Product" />
-          </Link>
-        </div>
-        <div style={{ margin: "10px" }}>
-          <ProdTextContainer>
-            <div>
-              <span style={{ fontWeight: "bold" }}>
-                {" "}
-                {props.item.Name}{" "}
-              </span>
-            </div>
-            <div style={{ fontSize: '13px', margin: '10px'}}>
-                {props.item.Description}
-            </div>
-            <div>
-              <span style={{ fontWeight: "bold" }}>Category</span>:&nbsp;
-              {props.item.Category}
-            </div>
-            <div>
-              <span style={{ fontWeight: "bold" }}>Price</span>: $
-              {props.item.Price}
-            </div>
-            <div>
-              <Rating ratings={props.item.Rating} />
+  return (
+    <ProdCard key={props.item.Name}>
+      <div style={{ backgroundColor: "white" }}>
+        <Link to="/product">
+          <img
+            style={{ width: "100%", height: "200px", objectFit: "contain" }}
+            src={props.item.Images[0]}
+            alt="Product"
+          />
+        </Link>
+      </div>
+      <div style={{ margin: "10px" }}>
+        <ProdTextContainer>
+          <div>
+            <span style={{ fontWeight: "bold" }}> {props.item.Name} </span>
+          </div>
+          <div style={{ fontSize: "13px", margin: "10px" }}>
+            {props.item.Description}
+          </div>
+          <div>
+            <span style={{ fontWeight: "bold" }}>Category</span>:&nbsp;
+            {props.item.Category}
+          </div>
+          <div>
+            <span style={{ fontWeight: "bold" }}>Price</span>: $
+            {props.item.Price}
+          </div>
+          <div>
+            <Rating ratings={props.item.Rating} />
+            <div
+              style={{
+                position: "absolute",
+                bottom: "-40px",
+                right: "-70px",
+                float: "right",
+              }}
+            >
               <AddToCartButton
-              productDetails={productDetails}
-              quantity={quantity}
+                productDetails={productDetails}
+                quantity={quantity}
               />
             </div>
-          </ProdTextContainer>
-        </div>
-      </ProdCard>
-    );
+          </div>
+        </ProdTextContainer>
+      </div>
+    </ProdCard>
+  );
 }
--- client/src/components/Styles/ProdTextContainer.styled.js
+++ client/src/components/Styles/ProdTextContainer.styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
 export const ProdTextContainer = styled.div`
-    text-align: left;
-`
+  text-align: left;
+  position: relative;
+`;
