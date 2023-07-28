b1c009294fec6a72bb7da9f54b796eefceed58a1
eshitasharma <eshitasharma0804@gmail.com>
2023-07-28 16:20:50 -0700

    added-link-to-share

--- client/src/components/ProductPage/ProductPage.js
+++ client/src/components/ProductPage/ProductPage.js
@@ -16,7 +16,8 @@ import RelatedItems from "./RelatedItems";
 function ProductPage() {
   // TODO: Add prop for product details
   const [quantity, setQuantity] = useState(1);
-  const shareUrl = window.location.pathname;
+  const shareUrl = window.location.origin + window.location.pathname;
+  const quote = "See this Awesome product";
   // https://stackoverflow.com/a/71247418
   const { state } = useLocation();
   const item = state.item || {};
@@ -83,7 +84,7 @@ function ProductPage() {
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
               />
             </svg>
-            <FacebookShareButton url={shareUrl}>
+            <FacebookShareButton url={shareUrl} quote={quote}>
               <FacebookIcon size={30} round={true}></FacebookIcon>
             </FacebookShareButton>
           </div>
