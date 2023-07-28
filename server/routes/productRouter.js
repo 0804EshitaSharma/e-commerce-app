7d70d71637c70d5496950cd63a79dda4fff4fe08
Danielle Chen <jt17@students.cs.ubc.ca>
2023-07-27 20:24:29 -0700

    extract helpers and resolve param type confusion

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -4,37 +4,37 @@ const Items = require("../models/itemSchema");
 
 router.get("/", async (req, res, next) => {
   try {
-    const categoryParams = req.query.category;
-    const priceParams = req.query.price;
-    const ratingParams = req.query.rating;
-    const searchParams = req.query.search;
-
     let itemData = await Items.find({});
 
-    if (categoryParams !== undefined) {
-      itemData = itemData.filter((item) =>
-        categoryParams.includes(item.category)
-      );
-    }
+    itemData = filterByCategory(itemData);
+    itemData = filterByPrice(itemData);
+    itemData = filterByRating(itemData);
+    itemData = filterBySearchText(itemData);
 
-    if (priceParams !== undefined) {
+    res.status(200).send(itemData);
+  } catch (err) {
+    console.log(err);
+  }
+
+  function filterBySearchText(itemData) {
+    let searchParams = req.query.search;
+    if (searchParams !== undefined) {
+      if (!Array.isArray(searchParams)) {
+        searchParams = [searchParams];
+      }
       itemData = itemData.filter((item) => {
-        if (item.price < 25) {
-          return priceParams.includes("Under$25");
-        } else if (item.price >= 25 && item.price < 50) {
-          return priceParams.includes("$25~$50");
-        } else if (item.price >= 50 && item.price < 100) {
-          return priceParams.includes("$50~$100");
-        } else if (item.price >= 100 && item.price < 200) {
-          return priceParams.includes("$100~$200");
-        } else if (item.price > 200) {
-          return priceParams.includes("Above$200");
-        }
-        return false;
+        return item.name.includes(searchParams);
       });
     }
+    return itemData;
+  }
 
+  function filterByRating(itemData) {
+    let ratingParams = req.query.rating;
     if (ratingParams !== undefined) {
+      if (!Array.isArray(ratingParams)) {
+        ratingParams = [ratingParams];
+      }
       itemData = itemData.filter((item) => {
         if (item.rating < 1) {
           return ratingParams.includes("Below1");
@@ -50,16 +50,43 @@ router.get("/", async (req, res, next) => {
         return false;
       });
     }
+    return itemData;
+  }
 
-    if (searchParams !== undefined) {
+  function filterByPrice(itemData) {
+    let priceParams = req.query.price;
+    if (priceParams !== undefined) {
+      if (!Array.isArray(priceParams)) {
+        priceParams = [priceParams];
+      }
       itemData = itemData.filter((item) => {
-        return item.name.includes(searchParams)
-      })
+        if (item.price < 25) {
+          return priceParams.includes("Under$25");
+        } else if (item.price >= 25 && item.price < 50) {
+          return priceParams.includes("$25~$50");
+        } else if (item.price >= 50 && item.price < 100) {
+          return priceParams.includes("$50~$100");
+        } else if (item.price >= 100 && item.price < 200) {
+          return priceParams.includes("$100~$200");
+        } else if (item.price > 200) {
+          return priceParams.includes("Above$200");
+        }
+        return false;
+      });
     }
+    return itemData;
+  }
 
-    res.status(200).send(itemData);
-  } catch (err) {
-    console.log(err);
+  function filterByCategory(itemData) {
+    let categoryParams = req.query.category;
+    if (categoryParams !== undefined) {
+      if (!Array.isArray(categoryParams)) {
+        categoryParams = [categoryParams];
+      }
+      itemData = itemData.filter((item) => categoryParams.includes(item.category)
+      );
+    }
+    return itemData;
   }
 });
 
