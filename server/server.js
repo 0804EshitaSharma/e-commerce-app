09ec2c0895773673804fa5a74123f71aabc0718b
eshitasharma <eshitasharma0804@gmail.com>
2023-07-27 23:29:12 -0700

    added-deploy-changes

--- server/server.js
+++ server/server.js
@@ -29,18 +29,10 @@ app.use("/products", productRouter);
 app.use("/user", userRouter);
 app.use("/order", ordersRouter);
 
-app.post("/order", async (req, res, next) => {
+app.post("/orders", async (req, res, next) => {
   const order = req.body;
-  console.log("Received order data on the server:", order);
   try {
-    const newOrder = await Orders.create(order);
-    // const userId = req.body.user;
-    // const validUserId = mongoose.Types.ObjectId(userId);
-    // const user = await Users.findById(validUserId);
-    // user.orders.push(newOrder._id);
-    // await user.save();
-    console.log("New order created:", newOrder);
-    // console.log("user's orders:", user.orders);
+    const newOrder = await Order.create(order);
     res.status(201).json(newOrder);
   } catch (error) {
     res.status(500).json({ error: error.message });
