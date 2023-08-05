2e6c91c5c224b15c1fb6de8890ef379caa29e0b3
eshitasharma <eshitasharma0804@gmail.com>
2023-08-05 01:09:19 -0700

    added-responsiveness

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -95,26 +95,8 @@ function Navbar({ name }) {
             </Link>
           )}
         </>
-        <div onClick={showMenu} className="user_menu">
-          <div className="user_name">
-            {!name ? "Guest" : name}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="icon"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </div>
-        </div>
-        <div>{showDropdown && <CustomDropdown />}</div>
+
+            <CustomDropdown name={name} />
 
         <div>
           <Link to={RoutePaths.Wishlist}>
