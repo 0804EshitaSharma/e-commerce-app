f884cc38d81170279aaf66ad52d1474a748fb0cc
eshitasharma <eshitasharma0804@gmail.com>
2023-08-07 20:35:06 -0700

    fixed-indentation

--- client/src/pages/front.js
+++ client/src/pages/front.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ProductSlideShow from "../components/Dashboard/Products/ProdSlideShow"
+import ProductSlideShow from "../components/Dashboard/Products/ProdSlideShow";
 import ProductContainer from "../components/Dashboard/Products/ProductContainer";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
@@ -7,20 +7,20 @@ import { products } from "../redux/item/itemSlice";
 import { getProdListAsync } from "../redux/item/itemSlice";
 
 const Front = () => {
-    const list = useSelector(products);
-    const dispatch = useDispatch();
+  const list = useSelector(products);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getProdListAsync(``));
-    }, []);
+  useEffect(() => {
+    dispatch(getProdListAsync(``));
+  }, []);
 
-    return (
-        <div style={{ backgroundColor: "white"}}>
-            <ProductSlideShow />
-            <h2>Our Products</h2>
-            <ProductContainer list={list}/>
-        </div>
-    )
-}
+  return (
+    <div style={{ backgroundColor: "white" }}>
+      <ProductSlideShow />
+      <h2>Our Products</h2>
+      <ProductContainer list={list} />
+    </div>
+  );
+};
 
-export default Front
+export default Front;
